8522638248399c313310e10ce89a894ea6359571
Petter Hagelund <pehag5384@uib.no>
2026-03-27 19:22:53 +0100

    Oppgave 1-3
    Hentet en random oppskrift fra API, og pressenterte det på siden med overskrift, bilde, kategori, igredienser og oppskrift

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 Mapping from MealDB Categories to TheCocktailDB drink ingredient
 You can customize or expand this object to suit your needs.
 */
+const mealInfo = document.getElementById('meal-container');
+
 const mealCategoryToCocktailIngredient = {
   Beef: "whiskey",
   Chicken: "gin",
@@ -48,9 +50,12 @@ function init() {
  Fetch a Random Meal from TheMealDB
  Returns a Promise that resolves with the meal object
  */
-function fetchRandomMeal() {
-    // Fill in
-}
+async function fetchRandomMeal() {
+  let response = await fetch ('https://www.themealdb.com/api/json/v1/1/random.php');
+  let randomMeal = await response.json();
+  // console.log(randomMeal)
+  return randomMeal
+  }
 
 /*
 Display Meal Data in the DOM
@@ -58,10 +63,49 @@ Receives a meal object with fields like:
   strMeal, strMealThumb, strCategory, strInstructions,
   strIngredientX, strMeasureX, etc.
 */
-function displayMealData(meal) {
-    // Fill in
+
+function getIngrediatArray(mealData) {
+  let ingrediantArray = [];
+
+  for (let index = 1; index <= 20; index++){ //øker index med 1 doe hver iterasjon
+    // console.log(index);
+    //ingrediantArray.push(mealData.strIngredient1)
+    //console.log('ingrediant1Test:',mealData.strIngredient1);
+    //console.log('tempArray:',ingrediantArray);
+    let ingrediant = mealData[`strIngredient${index}`];
+    let measure = mealData[`strMeasure${index}`];
+
+    if (ingrediant != '') {
+      ingrediantArray.push ({name:ingrediant,measure:measure});
+    }
+  }
+  return ingrediantArray;
+  console.log('log array:',ingrediantArray);
 }
 
+function displayMealData(meal) {
+  //console.log("3", meal.meals[0])
+
+  let data = meal.meals[0];
+  let ingrediantsArray = getIngrediatArray(data);
+console.log('ingredientsArray: ',ingrediantsArray)
+
+let ingrediantElement = ingrediantsArray.map(ingrediant => `<tr><td>${ingrediant.name}</td> <td>${ingrediant.measure}</td></tr>`) //går igjennom arrayet der ingrediant skriver ingrediensnavn og measure
+let elementString = ingrediantElement.join('') //gjør det om til string
+  console.log('data:',data);
+  mealInfo.innerHTML = `
+  <div>
+    <h2>${data.strMeal} </h2>
+    <img src = '${data.strMealThumb}'>
+    <h3>Category:</h3>
+    <p>${data.strCategory}</p>
+    <h3>Instructions:</h3>
+    <p>${data.strInstructions}</p>
+    <h3>Ingredients:</h3>
+    <table><tbody>${elementString}</tbody></table>
+  </div>
+  `
+}
 /*
 Convert MealDB Category to a TheCocktailDB Spirit
 Looks up category in our map, or defaults to 'cola'
@@ -77,7 +121,7 @@ Returns Promise that resolves to cocktail object
 We call https://www.thecocktaildb.com/api/json/v1/1/search.php?s=DRINK_INGREDIENT to get a list of cocktails
 Don't forget encodeURIComponent()
 If no cocktails found, fetch random
-*/
+*/-
 function fetchCocktailByDrinkIngredient(drinkIngredient) {
     // Fill in
 }
